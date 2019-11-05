47b518666ef1b9e726379cddc8b5719634e452a2
lhl-cpu <289041280@qq.com>
2019-11-05 21:51:32 +0800

    完善Gallery APP

---
+++ firstportfolio/gallery/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Gallery(models.Model):
+	descrtion = models.CharField(default='这里是作品简介' ,max_length=100)
+	image = models.ImageField(default='default.png',upload_to='images/')
+	title = models.CharField(default='作品标题' ,max_length=50)
+
+	def __str__(self):
+		return self.title
